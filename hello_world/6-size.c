6ddf479cfe88911efe8cecbef10724cadcfcfb4e
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-17 16:49:12 +0200

    printf %

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints the size of various types on the computer it is compiled and run on
+ * Return: 0 on success
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+}
