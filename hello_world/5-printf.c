1b95bf3f7a9a14d2a99e754bd184aae302fef048
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-17 16:32:13 +0200

    printf1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * Description:Prints "with proper grammar, but the outcome is a piece of art,"
  * Return: 0 on success
  */
 int main(void)
