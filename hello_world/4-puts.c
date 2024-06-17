d1e76b1fd6f0f793d17a69bb73e300af5fe5ccae
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-17 16:21:03 +0200

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * Return: 0 on success
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
