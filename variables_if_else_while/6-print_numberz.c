7e340c21ec04984983b52f0dbcf4c454c71d5e77
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-19 09:41:10 +0200

    n6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-int i;
-
-for (i = 0; i < 10; i++)
-{
-printf("%d", i);
-}
-putchar('\n');
-return (0);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
