2061979fd8165a7d869ca0526bedb39cfbf3f076
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-19 10:31:01 +0200

    putchar putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+char c;
+
+for (c = '0'; c < '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c < 'f'; c++)
+{
+putchar(c);
+}
+}
