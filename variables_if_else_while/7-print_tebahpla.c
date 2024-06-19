c6cfc4aabc899f2c53521461d9d71f73f14728f8
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-19 10:03:21 +0200

    c--

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse
+ * Return: 0 on success
+ */
+int main(void)
+{
+    char c;
+
+    for (c = 'z'; c >= 'a'; c--)
+    {
+        putchar(c);
+    }
+    putchar('\n');
+    return (0);
+}
