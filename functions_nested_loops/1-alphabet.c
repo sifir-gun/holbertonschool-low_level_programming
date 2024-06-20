0ce5acddae6e4c28d174c214fc9a938b0bd2e3e7
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-20 14:07:49 +0200

    print_alphabet void

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  * Description: prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0.
