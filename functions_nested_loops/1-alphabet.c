c4ef61b0b7a47f4caf628bf5b6aa33bdb7bf132b
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-20 13:51:16 +0200

    print_alphabet void

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+/* Fichier 1-alphabet.c ou un autre fichier source approprié */
+void print_alphabet(void)
+{
+    char c[26] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    for (i = 0; i < 26; i++)
+    {
+        _putchar(c[i]);
+    }
+    _putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,7 @@
 int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void); 
+
+#endif
