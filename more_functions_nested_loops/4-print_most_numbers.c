43b4cfd25c9044fb839eab6bd556566ca77bbceb
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-24 11:26:21 +0200

    for

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
     int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        if (i != 2 && i != 4)
-        {
-            _putchar(i + '0');
-        }
-    }
-    _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
 }
