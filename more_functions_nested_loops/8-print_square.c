d1a323bd110acd7519125d3c7f7573267b0a25aa
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-24 15:40:37 +0200

    if for for else

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - check the code
+ *
+ * Description: prints a square
+ * @size: size of the square
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size >0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
