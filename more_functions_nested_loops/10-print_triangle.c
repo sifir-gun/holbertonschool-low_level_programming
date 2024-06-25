26ae12414c05b2f8da7f2e0f45e3fcb36311d162
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-25 10:32:35 +0200

    ijk size

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,24 +9,24 @@
  */
 void print_triangle(int size)
 {
-    int i, j, k;
+	int i, j, k;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 1; i <= size; i++)
-    {
-        for (j = size - i; j > 0; j--)
-        {
-            _putchar(' ');
-        }
-        for (k = 0; k < i; k++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
