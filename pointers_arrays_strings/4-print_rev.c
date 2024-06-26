809c54a38effa90c530ff7ecc661d6fa507667f9
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-26 14:44:23 +0200

    while for

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,18 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+	int len = 0;
+	int i;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
