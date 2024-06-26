6e5df16a42ae81581dd1e476358f79fcdb8c7c36
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-26 09:57:03 +0200

    taille while

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,15 @@
 #include "main.h"
 /**
- * 
+ * _strlen - returns the length of a string
+ * @s: - pointer to a string
  */
+int _strlen(char *s)
+{
+	int taille = 0;
+	while (*s != '\0')
+	{
+	taille++;
+	s++;
+	}
+	return (taille);
+}
