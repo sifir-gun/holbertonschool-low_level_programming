47b363c882a7d5e139908c589a2763fa2c912e3f
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-27 16:49:29 +0200

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,38 +3,46 @@
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
- *
+ * 
  * Return: the integer value of the string
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int started = 0;
+    int sign = 1;
+    int result = 0;
+    int started = 0;
 
-	while (*s)
-	{
-		if (*s == '-')
-		{
-			if (started)
-				break;
-			sign = -sign;
-		}
-		else if (*s == '+')
-		{
-			if (started)
-				break;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			started = 1;
-			result = result * 10 + (*s - '0');
-		}
-		else if (started)
-		{
-			break;
-		}
-		s++;
-	}
-	return (result * sign);
+    while (*s)
+    {
+        if (*s == '-')
+        {
+            if (started)
+                break;
+            sign = -sign;
+        }
+        else if (*s == '+')
+        {
+            if (started)
+                break;
+        }
+        else if (*s >= '0' && *s <= '9')
+        {
+            if (result > (2147483647 - (*s - '0')) / 10)
+            {
+                // Overflow detection
+                if (sign == 1)
+                    return 2147483647;
+                else
+                    return -2147483648;
+            }
+            started = 1;
+            result = result * 10 + (*s - '0');
+        }
+        else if (started)
+        {
+            break;
+        }
+        s++;
+    }
+    return (result * sign);
 }
