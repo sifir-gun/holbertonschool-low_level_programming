773188da8235973190871b518a30d23f4ec05710
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-27 16:54:37 +0200

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
@@ -8,40 +7,40 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int started = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-        {
-            if (started)
-                break;
-            sign = -sign;
-        }
-        else if (*s == '+')
-        {
-            if (started)
-                break;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            if (result > (2147483647 - (*s - '0')) / 10)
-            {
-                if (sign == 1)
-                    return 2147483647;
-                else
-                    return -2147483648;
-            }
-            started = 1;
-            result = result * 10 + (*s - '0');
-        }
-        else if (started)
-        {
-            break;
-        }
-        s++;
-    }
-    return (result * sign);
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			if (started)
+				break;
+			sign = -sign;
+		}
+		else if (*s == '+')
+		{
+			if (started)
+				break;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			if (result > (2147483647 - (*s - '0')) / 10)
+			{
+				if (sign == 1)
+					return 2147483647;
+				else
+					return -2147483648;
+			}
+			started = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
+	return (result * sign);
 }
