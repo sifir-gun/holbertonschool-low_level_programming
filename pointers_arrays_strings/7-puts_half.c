57dbea9148ed623e80efc7a91c87214abc0768d2
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-27 14:37:27 +0200

    while for

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,18 +5,18 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, n, i;
+	int len = 0, n, i;
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    n = (len + 1) / 2;
+	n = (len + 1) / 2;
 
-    for (i = n; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
