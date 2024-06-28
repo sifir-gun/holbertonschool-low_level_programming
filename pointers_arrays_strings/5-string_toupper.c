44aa62d87130b2d7a41f411224c5baa2de8e2744
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-28 16:31:08 +0200

    ex 5 int for return

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the string to be modified
+ *
+ * Return: a pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
