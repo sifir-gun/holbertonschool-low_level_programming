a0044ed7e710d463ab0c92e353af0eba2e415c1f
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-28 10:24:52 +0200

    char while while i return dest

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * 
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*i != '\0')
+	{
+		i++;
+	}
+
+	while (*src != '\0')
+	{
+		*i = *src;
+		i++;
+		src++;
+	}
+	i = '\0';
+	return (dest);
+}
