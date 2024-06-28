68cababe421c3f1cec58e37a350ca0fb1ba5d961
sifir-gun <guney.tasdelen74@gmail.com>
2024-06-28 16:55:30 +0200

    ex 6v1 int if for if if return

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: the string to be modified
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to be modified
+ *
+ * Return: a pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+	}
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+
+	return (str);
+}
