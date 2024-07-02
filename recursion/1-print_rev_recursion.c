0886af671fb7e88934d0c8779eecc1178b6a75af
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-02 16:29:41 +0200

    test

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * _print_rev_recursion - prints a string rev, followed by a new line
  * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
