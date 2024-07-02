db0e59085c17a2a3fd4c4fb034e1c6d0c16d9f0c
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-02 16:23:22 +0200

    projet 16 - exercice 2

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: the string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
