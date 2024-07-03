1a1fb9a874400aa2cd4c3302360116dd5cbf31ad
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-03 14:53:27 +0200

    projet 16 - exercice 6 - int if return return int if return if return return

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,1 +1,35 @@
 #include "main.h"
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if the number is a prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - checks if a number is a prime by testing divisors
+ * @n: the number to check
+ * @i: the current divisor being tested
+ *
+ * Return: 1 if the number is a prime, 0 otherwise
+ */
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (check_prime(n, i + 1));
+}
