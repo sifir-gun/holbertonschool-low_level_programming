62fb30e8fdced22597d3b36ab75cc63825644af8
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-03 11:49:21 +0200

    projet 16 - exercice 5 - int if return int if if return return

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,1 +1,29 @@
 #include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * sqrt_helper - helps to find the natural square root of a number
+ * @n: the number to calculate the square root for
+ * @i: the current number being tested as the square root
+ *
+ * Return: the natural square root of n, or -1 if n does not have one
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sqrt_helper(n, 0));
+}
+int _sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (sqrt_helper(n, i + 1));
+}
