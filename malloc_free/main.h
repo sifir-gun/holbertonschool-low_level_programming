1de178bca9023ae8e2047bdb8eb8ed9a167d28c4
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-09 15:54:17 +0200

    projet 18 - exercice 3

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -14,7 +14,7 @@ void print_triangle(int size);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-
+int **alloc_grid(int width, int height);
 
 
 #endif
