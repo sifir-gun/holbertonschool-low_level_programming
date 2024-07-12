e8e5f38af5b5e8339850f30eed25300605be46e0
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-12 16:10:30 +0200

    project structures & typedef exercice 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+
+}
