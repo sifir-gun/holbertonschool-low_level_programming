23b92c90edcf1fd9f410c6a3acd1c7c856e2ba56
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-12 16:13:37 +0200

    project structures & typedef exercice 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,6 +1,10 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_dog - Frees memory allocated for a dog_t struct.
+ * @d: Pointer to the dog_t struct to free.
+ */
 void free_dog(dog_t *d)
 {
 	if (d)
