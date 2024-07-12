2d8557c90bede7adf2ce37fa8694be2dba11411f
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-12 13:02:06 +0200

    project structures & typedef exercice 0 delete newline

--- structures_typedef/0-main.c
+++ structures_typedef/0-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
  * main - check the code
  *
@@ -8,11 +7,10 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	struct dog my_dog;
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
 }
