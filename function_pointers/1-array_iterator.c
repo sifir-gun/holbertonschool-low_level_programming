9a9104ff32d624f2007e4bb5b49ab44f7b313566
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-16 09:57:08 +0200

    C - Function pointers - exercice 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * array_iterator -Exécute une fonction donnée sur chaque élément du tableau
+ * @array: le tableau d'entiers
+ * @size: la taille du tableau
+ * @action: un pointeur vers la fonction à utiliser pour chaque élément
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		size_t i;
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
