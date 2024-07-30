dd9e6fc879f1b105e6e567fff37b50f87b262d28
sifir-gun <guney.tasdelen74@gmail.com>
2024-07-30 10:10:21 +0200

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    while (h)
-    {
-        if (h->str)
-            printf("[%u] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
-        h = h->next;
-        nodes++;
-    }
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		nodes++;
+	}
 
-    return (nodes);
+	return (nodes);
 }
