7ecd3272ec455ceb538e0fbf8727a0600738b17c
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-02 17:19:08 +0200

    add task + betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
+
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
+ * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Double pointer to the head of the list.
  * @str: String to be duplicated and added to the new node.
  *
