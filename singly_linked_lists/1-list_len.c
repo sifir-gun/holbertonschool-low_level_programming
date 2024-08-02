a4c2bdc73b389d2c072003d2c138ebb5949794a6
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-02 17:14:14 +0200

    add task 1 + betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,16 +7,15 @@
  * Return: The number of elements in the list.
  */
 
- size_t list_len(const list_t *h)
- {
+size_t list_len(const list_t *h)
+{
 	size_t count = 0;
 
-	while ( h != NULL)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
 
 	return (count);
- }
- 
+}
