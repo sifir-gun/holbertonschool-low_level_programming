db1c78cdef8f5492e468c2483fc9348d13be75c4
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-02 17:21:38 +0200

    add task 4 + betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,13 +6,13 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
